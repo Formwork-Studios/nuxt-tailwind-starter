f420015e953a0726bb3de2f646772f33af34be32
mikey <mforest@protonmail.com>
2023-10-21 17:44:46 -0400

    imported css

--- app.vue
+++ app.vue
@@ -5,6 +5,8 @@
 </template>
 
 <script setup lang="ts">
+import '~/assets/css/main.css';
+
 useHead({
   titleTemplate: (titleChunk) => {
     return titleChunk ? `${titleChunk} - Nuxt Tailwind Starter` : 'Nuxt Tailwind Starter';

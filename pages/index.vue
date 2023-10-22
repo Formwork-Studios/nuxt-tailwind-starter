1eadf103ae564cab969bc98dc683e138afba9b66
mikey <mforest@protonmail.com>
2023-10-22 06:16:32 -0400

    Cleaning up

--- pages/index.vue
+++ pages/index.vue
@@ -3,8 +3,8 @@
 <template>
   <div>
     <h1 class="text-primary">H1 with primary color</h1>
-    <h2 class="text-secondary-dark">H2 with secondary dark  variation</h2>
-    <p>This is a paragraph</p>
+    <h2 class="text-secondary-dark">H2 with secondary dark variation</h2>
+    <p>This is a paragraph.</p>
     <Btn :icon="false" :to="'about'" />
   </div>
 </template>

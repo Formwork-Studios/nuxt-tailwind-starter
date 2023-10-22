7929f5f0e3f79cbb3ddaef185c6a98bbea0304ac
mikey <mforest@protonmail.com>
2023-10-22 05:48:34 -0400

    removed items from gitignore, updated nuxt config

---
+++ _nuxt/.output/server/chunks/app/_nuxt/island-renderer-309840cb.mjs
@@ -0,0 +1,39 @@
+import { defineComponent, createVNode } from 'vue';
+import { c as createError } from '../server.mjs';
+import '../../nitro/node-server.mjs';
+import 'node:http';
+import 'node:https';
+import 'fs';
+import 'path';
+import 'node:fs';
+import 'node:url';
+import 'unhead';
+import '@unhead/shared';
+import 'vue/server-renderer';
+
+const components_islands = {};
+const islandComponents = /* @__PURE__ */ Object.freeze({
+  __proto__: null,
+  default: components_islands
+});
+const islandRenderer = /* @__PURE__ */ defineComponent({
+  props: {
+    context: {
+      type: Object,
+      required: true
+    }
+  },
+  setup(props) {
+    const component = islandComponents[props.context.name];
+    if (!component) {
+      throw createError({
+        statusCode: 404,
+        statusMessage: `Island component not found: ${props.context.name}`
+      });
+    }
+    return () => createVNode(component || "span", { ...props.context.props, "nuxt-ssr-component-uid": "" });
+  }
+});
+
+export { islandRenderer as default };
+//# sourceMappingURL=island-renderer-309840cb.mjs.map

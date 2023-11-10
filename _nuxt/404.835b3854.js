5fceb6d68ecbe39c82e8d2dd83b2e037592f38d7
mikey <mforest@protonmail.com>
2023-11-10 06:30:37 -0500

    Deploying static site

--- _nuxt/404.835b3854.js
+++
@@ -1,1 +0,0 @@
-import{_ as s}from"./MainNav.vue.9ac6890a.js";import{_ as t}from"./github.0e6d1b2b.js";import{c,a,b as r,r as n,o as _}from"./entry.f1b8b500.js";const i={},f={class:"flex flex-col items-center"},l={class:"container px-2"};function d(e,m){const o=s;return _(),c("div",f,[a(o),r("div",l,[n(e.$slots,"default")])])}const h=t(i,[["render",d]]);export{h as default};
